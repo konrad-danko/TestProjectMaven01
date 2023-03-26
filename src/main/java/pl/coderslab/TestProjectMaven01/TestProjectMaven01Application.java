ccd1a7580d7dce6d5c48538e0214ea2bcfef5db7
konrad-danko <konrad.danko@gmail.com>
2023-03-26 17:06:55 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/TestProjectMaven01/TestProjectMaven01Application.java
@@ -0,0 +1,13 @@
+package pl.coderslab.TestProjectMaven01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectMaven01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectMaven01Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/TestProjectMaven01/TestProjectMaven01ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.TestProjectMaven01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectMaven01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
